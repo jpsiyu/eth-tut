37ca39dbf7ac58e8b8d5e355bb11e332261ecda4
wusiyu <stephenwu2019@163.com>
2019-11-08 15:31:05 +0800

    add tut about contract

---
+++ tut12/contract_deploy.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"eth-tut/tut12/store"
+	"fmt"
+	"log"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	client, err := ethclient.Dial("https://rinkeby.infura.io")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	privateKey, err := crypto.HexToECDSA("fad9c8855b740a0b7ed4c221dbad0f33a83a49cad6b3fe8d5817ac83d38b6a19")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+	}
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	auth := bind.NewKeyedTransactor(privateKey)
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(0)
+	auth.GasLimit = uint64(300000)
+	auth.GasPrice = gasPrice
+
+	input := "1.0"
+
+	address, tx, instance, err := store.DeployStore(auth, client, input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(address.Hex())   //0xB4323fe368E930345AA956E2ea50E0C345988521
+	fmt.Println(tx.Hash().Hex()) //0x9f9dbcd1abead98ddb47ab0b068a2143885fbe6bc0e6434b88dd2e7bf709c076
+	_ = instance
+}
---
+++ tut14/contract_read.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"eth-tut/tut12/store"
+	"fmt"
+	"log"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	client, err := ethclient.Dial("https://rinkeby.infura.io")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	address := common.HexToAddress("0xB4323fe368E930345AA956E2ea50E0C345988521")
+	instance, err := store.NewStore(address, client)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("contract is loaded")
+
+	version, err := instance.Version(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("version:", version)
+}
